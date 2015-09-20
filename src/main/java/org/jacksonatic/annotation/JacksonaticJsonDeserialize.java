7d246d2dce0bd960dc65c458c2392acd4f0b4a9d
Morgan Renou <mrenou@gmail.com>
2015-09-20 12:27:03 +0200

    add JsonSerialize and JsonDeserialize annotations

---
+++ src/main/java/org/jacksonatic/annotation/JacksonaticJsonDeserialize.java
@@ -0,0 +1,140 @@
+package org.jacksonatic.annotation;
+
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.util.Converter;
+
+import java.lang.annotation.Annotation;
+
+public class JacksonaticJsonDeserialize implements JsonDeserialize {
+
+    private Class<? extends JsonDeserializer<?>> using = JsonDeserializer.None.class;
+
+    private Class<? extends JsonDeserializer<?>> contentUsing = JsonDeserializer.None.class;
+
+    private Class<? extends KeyDeserializer> keyUsing = KeyDeserializer.None.class;
+
+    private Class<?> builder = Void.class;
+
+    private Class<? extends Converter<?, ?>> converter = Converter.None.class;
+
+    private Class<? extends Converter<?, ?>> contentConverter = Converter.None.class;
+
+    private Class<?> as = Void.class;
+
+    private Class<?> keyAs = Void.class;
+
+    private Class<?> contentAs = Void.class;
+
+    @Override
+    public Class<? extends JsonDeserializer<?>> using() {
+        return using;
+    }
+
+    @Override
+    public Class<? extends JsonDeserializer<?>> contentUsing() {
+        return contentUsing;
+    }
+
+    @Override
+    public Class<? extends KeyDeserializer> keyUsing() {
+        return keyUsing;
+    }
+
+    @Override
+    public Class<?> builder() {
+        return builder;
+    }
+
+    @Override
+    public Class<? extends Converter<?, ?>> converter() {
+        return converter;
+    }
+
+    @Override
+    public Class<? extends Converter<?, ?>> contentConverter() {
+        return contentConverter;
+    }
+
+    @Override
+    public Class<?> as() {
+        return as;
+    }
+
+    @Override
+    public Class<?> keyAs() {
+        return keyAs;
+    }
+
+    @Override
+    public Class<?> contentAs() {
+        return contentAs;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return JsonDeserialize.class;
+    }
+
+    public static Builder jsonDeserialize() {
+        return new Builder();
+    }
+
+    public static class Builder implements AnnotationBuilder {
+
+        private JacksonaticJsonDeserialize jsonDeserialize = new JacksonaticJsonDeserialize();
+
+        public Builder using(Class<? extends JsonDeserializer<?>> using) {
+            jsonDeserialize.using = using;
+            return this;
+        }
+
+        public Builder contentUsing(Class<? extends JsonDeserializer<?>> contentUsing) {
+            jsonDeserialize.contentUsing = contentUsing;
+            return this;
+        }
+
+        public Builder keyUsing(Class<? extends KeyDeserializer> keyUsing) {
+            jsonDeserialize.keyUsing = keyUsing;
+            return this;
+        }
+
+        public Builder builder(Class<?> builder) {
+            jsonDeserialize.builder = builder;
+            return this;
+        }
+
+        public Builder converter(Class<? extends Converter<?, ?>> converter) {
+            jsonDeserialize.converter = converter;
+            return this;
+        }
+
+        public Builder contentConverter(Class<? extends Converter<?, ?>> contentConverter) {
+            jsonDeserialize.contentConverter = contentConverter;
+            return this;
+        }
+
+        public Builder as(Class<?> as) {
+            jsonDeserialize.as = as;
+            return this;
+        }
+
+        public Builder keyAs(Class<?> keyAs) {
+            jsonDeserialize.keyAs = keyAs;
+            return this;
+        }
+
+        public Builder contentAs(Class<?> contentAs) {
+            jsonDeserialize.contentAs = contentAs;
+            return this;
+        }
+
+        @Override
+        public Annotation build() {
+            return jsonDeserialize;
+        }
+    }
+}
---
+++ src/main/java/org/jacksonatic/annotation/JacksonaticJsonSerialize.java
@@ -0,0 +1,161 @@
+package org.jacksonatic.annotation;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.util.Converter;
+
+import java.lang.annotation.Annotation;
+
+public class JacksonaticJsonSerialize implements JsonSerialize {
+
+    private Class<? extends JsonSerializer<?>> using = JsonSerializer.None.class;
+
+    private Class<? extends JsonSerializer<?>> contentUsing = JsonSerializer.None.class;
+
+    private Class<? extends JsonSerializer<?>> keyUsing = JsonSerializer.None.class;
+
+    private Class<? extends JsonSerializer<?>> nullsUsing = JsonSerializer.None.class;
+
+    private Class<?> as = Void.class;
+
+    private Class<?> keyAs = Void.class;
+
+    private Class<?> contentAs = Void.class;
+
+    private Typing typing = Typing.DEFAULT_TYPING;
+
+    private Class<? extends Converter<?, ?>> converter = Converter.None.class;
+
+    private Class<? extends Converter<?, ?>> contentConverter = Converter.None.class;
+
+    private Inclusion include = Inclusion.DEFAULT_INCLUSION;
+
+    @Override
+    public Class<? extends JsonSerializer<?>> using() {
+        return using;
+    }
+
+    @Override
+    public Class<? extends JsonSerializer<?>> contentUsing() {
+        return contentUsing;
+    }
+
+    @Override
+    public Class<? extends JsonSerializer<?>> keyUsing() {
+        return keyUsing;
+    }
+
+    @Override
+    public Class<? extends JsonSerializer<?>> nullsUsing() {
+        return nullsUsing;
+    }
+
+    @Override
+    public Class<?> as() {
+        return as;
+    }
+
+    @Override
+    public Class<?> keyAs() {
+        return keyAs;
+    }
+
+    @Override
+    public Class<?> contentAs() {
+        return contentAs;
+    }
+
+    @Override
+    public Typing typing() {
+        return typing;
+    }
+
+    @Override
+    public Class<? extends Converter<?, ?>> converter() {
+        return converter;
+    }
+
+    @Override
+    public Class<? extends Converter<?, ?>> contentConverter() {
+        return contentConverter;
+    }
+
+    @Override
+    public Inclusion include() {
+        return include;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return JsonSerialize.class;
+    }
+
+    public static Builder jsonSerialize() {
+        return new Builder();
+    }
+
+    public static class Builder implements AnnotationBuilder {
+
+        private JacksonaticJsonSerialize jsonSerialize = new JacksonaticJsonSerialize();
+
+        public Builder using(Class<? extends JsonSerializer<?>> using) {
+            jsonSerialize.using = using;
+            return this;
+        }
+
+        public Builder contentUsing(Class<? extends JsonSerializer<?>> contentUsing) {
+            jsonSerialize.contentUsing = contentUsing;
+            return this;
+        }
+
+        public Builder keyUsing(Class<? extends JsonSerializer<?>> keyUsing) {
+            jsonSerialize.keyUsing = keyUsing;
+            return this;
+        }
+
+        public Builder nullsUsing(Class<? extends JsonSerializer<?>> nullsUsing) {
+            jsonSerialize.nullsUsing = nullsUsing;
+            return this;
+        }
+
+        public Builder as(Class<?> as) {
+            jsonSerialize.as = as;
+            return this;
+        }
+
+        public Builder keyAs(Class<?> keyAs) {
+            jsonSerialize.keyAs = keyAs;
+            return this;
+        }
+
+        public Builder contentAs(Class<?> contentAs) {
+            jsonSerialize.contentAs = contentAs;
+            return this;
+        }
+
+        public Builder typing(Typing typing) {
+            jsonSerialize.typing = typing;
+            return this;
+        }
+
+        public Builder converter(Class<? extends Converter<?, ?>> converter) {
+            jsonSerialize.converter = converter;
+            return this;
+        }
+
+        public Builder contentConverter(Class<? extends Converter<?, ?>> contentConverter) {
+            jsonSerialize.contentConverter = contentConverter;
+            return this;
+        }
+
+        public Builder include(Inclusion include) {
+            jsonSerialize.include = include;
+            return this;
+        }
+
+        @Override
+        public Annotation build() {
+            return jsonSerialize;
+        }
+    }
+}
